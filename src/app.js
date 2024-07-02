3de28e150e8d321f50e99dc34f7065b8f94ce36e
aadityaghimire03 <aadityaghimire03@gmail.com>
2024-07-01 22:57:34 -0400

    Project set up and basic template.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,53 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC1aZ_G6Jg7BPmbebvikc_hv9gUfwqxW9I",
+    authDomain: "uiuclibrarymap.firebaseapp.com",
+    projectId: "uiuclibrarymap",
+    storageBucket: "uiuclibrarymap.appspot.com",
+    messagingSenderId: "501121734474",
+    appId: "1:501121734474:web:f4e9d04ca6dbfb149d53b3",
+    measurementId: "G-ETCQP8V9W9"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+// Sign in function
+function signIn() {
+    auth.signInWithPopup(provider)
+        .then((result) => {
+            const user = result.user;
+            console.log(`User ${user.displayName} signed in`);
+            alert(`User ${user.displayName} signed in`);
+        })
+        .catch((error) => {
+            console.error(error);
+            alert(`Error: ${error.message}`);
+        });
+}
+
+// Sign out function
+function signOutUser() {
+    auth.signOut().then(() => {
+        console.log('User signed out');
+        alert('User signed out');
+    }).catch((error) => {
+        console.error(error);
+        alert(`Error: ${error.message}`);
+    });
+}
+
 // Initialize and add the map
 function initMap() {
-    // The location of UIUC
     const uiuc = { lat: 40.1019523, lng: -88.2271615 };
-    // The map, centered at UIUC
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 15,
         center: uiuc,
     });
-    // The marker, positioned at UIUC
     const marker = new google.maps.Marker({
         position: uiuc,
         map: map,
@@ -22,4 +62,8 @@ function loadScript() {
     document.head.appendChild(script);
 }
 
-window.onload = loadScript;
+window.onload = () => {
+    loadScript();
+    document.getElementById('login').addEventListener('click', signIn);
+    document.getElementById('logout').addEventListener('click', signOutUser);
+};
